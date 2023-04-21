24a30dc635972aab5227169745ee27f786383802
NJUCO <122616369+NESTLEJUCO@users.noreply.github.com>
2023-04-21 10:28:34 -0600

    Uploading Revised Version 4
    Revised Banner to Slideshow Banner using JS

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let slideIndex = 0;
 let itemsArray = document.getElementsByClassName("user-input-quantity");
 let priceArray = document.querySelectorAll(".subtotal");
 let totalPrice = 0;
+
+showSlides();
 
 Array.prototype.forEach.call(itemsArray, update);
 
@@ -26,4 +29,23 @@ function updateTotal() {
     else totalPrice += parseFloat(element.innerHTML.slice(0));
     document.getElementById("total").innerHTML = "$" + totalPrice.toFixed(2);
   });
+}
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 3000);
 }
